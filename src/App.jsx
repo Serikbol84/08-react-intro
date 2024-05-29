f204c16c3b08456cb9ff33968f7badac5f75dc97
Serikbol <sh.serikbol@gmail.com>
2024-05-29 18:14:16 +0500

    first init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Components/Header/Header'
+// import Main from './Components/Main/Main'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+function Header () {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="header-logo-1">medium alike</div>
+                <div className="header-logo-2">Hello, world!</div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
