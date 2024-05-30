620828f470fb8139bb2ad099083f6a64eecce3d1
Serikbol <sh.serikbol@gmail.com>
2024-05-30 14:19:40 +0500

    changed text in third news-list

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -138,8 +138,10 @@ const Main = () => {
                                 </a>
                             </h1>
                             <p>
-                                You not only learn more Python by implementing what you already know but, 
-                                in the end, you can see how all your hard work pays off.
+                            Recently, I’ve been automating tasks more than often due to my lack of time. Thanks to that I have 5 
+                            new projects that I classified as beginner, intermediate, and advanced. <br />You’ll find links to the full 
+                            scripts and tutorials to solve each project. Also, I’m leaving a challenge to each of them to test your Python 
+                            skills.To make things easier, I already created a template for a cover letter. Here’s how the template we’ll use looks...
                             </p>
                         </div>
 
