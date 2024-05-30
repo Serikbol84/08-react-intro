0eeec4bde3646831838bf7d619e1e3c5c6f08aff
Serikbol <sh.serikbol@gmail.com>
2024-05-30 13:03:54 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Header from './Components/Header/Header'
-// import Main from './Components/Main/Main'
+import Main from './Components/Main/Main'
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <Main />
     </div>
   );
 }
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,2 +1,24 @@
+import author from '../../img/author.svg'
 
+const Main = () => {
+    return (<section className="main">
+        <div className="container">
+            <div className="news-list">
+                <div className="news-block">
+                    <div className="news">
+                        <div className="authors">
+                            <div className="auth-logo">
+                                <img src={author} alt="" />
+                            </div>
+                            <p>
 
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>);
+}
+ 
+export default Main;
